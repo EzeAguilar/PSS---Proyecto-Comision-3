6746f8ea5a3735dcffbfda560e5e90dbf64aaa71
Juan Ignacio <juanirmariani@gmail.com>
2024-09-30 17:35:43 -0300

    se agrego insert y update para pacientes en data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,3 +21,18 @@ export async function fetchPatient(id: string): Promise<Patient> {
     });
     return patient[0];
 }
+
+export async function insertPatient(patient: Patient): Promise<void> {
+    await sql`
+    INSERT INTO pacientes (ID_Paciente, nombre, apellido, fecha_nac, domicilio, telefono, email, deshabilitado)
+    VALUES (${patient.ID_Paciente}, ${patient.nombre}, ${patient.apellido}, ${patient.fecha_nac}, ${patient.domicilio}, ${patient.telefono}, ${patient.email}, ${patient.deshabilitado})
+    `;
+}
+
+export async function updatePatient(patient: Patient): Promise<void> {
+    await sql`
+    UPDATE pacientes
+    SET nombre = ${patient.nombre}, apellido = ${patient.apellido}, fecha_nac = ${patient.fecha_nac}, domicilio = ${patient.domicilio}, telefono = ${patient.telefono}, email = ${patient.email}, deshabilitado = ${patient.deshabilitado}
+    WHERE ID_Paciente = ${patient.ID_Paciente}
+    `;
+}
